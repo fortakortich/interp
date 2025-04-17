30dc1265637030aee56653d8c2fdbb220627b325
fortakortich <157271569+fortakortich@users.noreply.github.com>
2025-04-17 06:48:04 +0500

    Add files via upload

---
+++ interpress/model.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv("universities_extended_final_large_ru.csv")
+
+def recommend_universities(inputs):
+    filtered = df[
+        (df["Минимальный GPA"] <= inputs["gpa"]) &
+        (df["Минимальный IELTS"] <= inputs["ielts"]) &
+        (df["Минимальный SAT"] <= inputs["sat"]) &
+        (df["Минимальный AP"] <= inputs["ap"]) &
+        (df["Минимальный бюджет ($)"] <= inputs["budget"]) &
+        (df["Направление"] == inputs["major"]) &
+        (df["Климат"] == inputs["climate"]) &
+        (df["Уровень проектов"] <= inputs["project_level"])
+    ]
+
+    if filtered.empty:
+        return []
+
+    return filtered.sample(n=min(10, len(filtered)))[[
+        "Название университета", "Страна", "Город", "Гранты", "Стоимость проживания",
+        "Краткое описание", "Язык обучения", "QS Рейтинг в стране", "QS Рейтинг в мире"
+    ]].to_dict(orient="records")
